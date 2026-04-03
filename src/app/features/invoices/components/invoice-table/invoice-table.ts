db9d75366eef6815b68b856d19a4560398dcc74b
Naassom Pedro <naassom7@gmail.com>
2026-04-03 12:36:06 -0300

    feat: adiciona a funcionalidade de excluir um registro

--- src/app/features/invoices/components/invoice-table/invoice-table.ts
+++ src/app/features/invoices/components/invoice-table/invoice-table.ts
@@ -9,6 +9,7 @@ import { InvoiceService } from '../../../../core/services/invoice.service';
 import { TagModule } from 'primeng/tag';
 import { CurrencyPipe } from '@angular/common';
 import { DatePipe } from '@angular/common'; 
+import { startWith, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-invoice-table',
@@ -20,23 +21,27 @@ export class InvoiceTable {
 
   private readonly invoiceService = inject(InvoiceService);
   private readonly invoiceEventsService = inject(InvoiceEventsService);
-  invoicesList$ = this.invoiceEventsService.updateInvoices$;
   private readonly destroyRef = inject(DestroyRef);
 
+  invoicesList$ = this.invoiceEventsService.refreshInvoices$.pipe(
+    startWith(void 0),
+    switchMap(() => this.invoiceService.getAll())
+  );
 
   ngOnInit(): void {
-    this.invoiceEventsService.refreshInvoices$
-      .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe(() => {
-        this.invoicesList$ = this.invoiceService.getAll();
+    // this.invoiceEventsService.refreshInvoices$
+    //   .pipe(takeUntilDestroyed(this.destroyRef))
+    //   .subscribe(() => {
+    //     this.invoicesList$ = this.invoiceService.getAll();
 
-
-
-      });
+    //   });
   }
 
   delete(invoice: InvoiceItemDto) {
-    alert("vai deletar")
+    this.invoiceService.delete(invoice.id).subscribe(() => {
+      console.log(`Invoice with id ${invoice.id} deleted successfully.`);
+      this.invoiceEventsService.emitRefreshInvoices();
+    });
   }
 
 }
